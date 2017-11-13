e41cf463798048d7edbdaff0f71d85e2e2c86e75
hami <yingkuang.chen@gmail.com>
2017-11-13 12:11:01 +0800

    doctrine odm

---
+++ Documents/Student.php
@@ -0,0 +1,88 @@
+<?php
+namespace Documents;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+
+/** @ODM\Document */
+class Student
+{
+    /** @ODM\Id */
+    private $id;
+
+    /** @ODM\Field(type="integer") */
+    private $key;
+
+    /** @ODM\Field(type="string") */
+    private $name;
+
+    /** @ODM\Field(type="string") */
+    private $person_id;
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPersonId()
+    {
+        return $this->person_id;
+    }
+
+    /**
+     * @param mixed $person_id
+     */
+    public function setPersonId($person_id)
+    {
+        $this->person_id = $person_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param mixed $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+
+}
---
+++ add.php
@@ -0,0 +1,20 @@
+<?php
+
+include "config.php";
+
+// 隨機取 5個 姓名
+$nameStr = file_get_contents('./data/name-random.txt');
+$nameArr = explode("\n", $nameStr);
+$randKey = array_rand($nameArr, 5);
+
+
+foreach ($randKey as $key) {
+    $tempArr = explode(',', $nameArr[$key]);
+    $student = new \Documents\Student();
+    $student->setKey($key);
+    $student->setName(trim($tempArr[0]));
+    $student->setPersonId(trim($tempArr[1]));
+    $dm->persist($student);
+}
+
+$dm->flush();
